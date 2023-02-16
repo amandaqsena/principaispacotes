7b6f572ce42b6d291ab9421512c47eb83b13d73e
F0655369 Amanda Queiroz Sena <amanda.queiroz@bb.com.br>
2023-02-16 16:56:31 -0300

    sem usar stream

--- src/main/java/principaispacotes/javaio/TesteEscritaPrintStream.java
+++ src/main/java/principaispacotes/javaio/TesteEscritaPrintStream.java
@@ -2,11 +2,14 @@ package principaispacotes.javaio;
 
 import java.io.IOException;
 import java.io.PrintStream;
+import java.io.PrintWriter;
 
 public class TesteEscritaPrintStream {
     public static void main(String[] args) throws IOException{
         
-        PrintStream ps = new PrintStream("src/main/resources/output.txt");
+        //PrintStream ps = new PrintStream("src/main/resources/output.txt");
+        PrintWriter ps = new PrintWriter("src/main/resources/output.txt");
+
 
         ps.println("Oi");
         ps.println();
