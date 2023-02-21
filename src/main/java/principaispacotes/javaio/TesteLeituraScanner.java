483666bdad1b489c5ca8d98b89f779aee943a022
F0655369 Amanda Queiroz Sena <amanda.queiroz@bb.com.br>
2023-02-21 16:52:43 -0300

    faz print formatado

--- src/main/java/principaispacotes/javaio/TesteLeituraScanner.java
+++ src/main/java/principaispacotes/javaio/TesteLeituraScanner.java
@@ -2,6 +2,8 @@ package principaispacotes.javaio;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.Arrays;
+import java.util.Locale;
 import java.util.Scanner;
 
 public class TesteLeituraScanner {
@@ -12,6 +14,27 @@ public class TesteLeituraScanner {
         while(scanner.hasNextLine()) {
             String linha = scanner.nextLine();
             System.out.println(linha);
+
+            //String[] valores = linha.split(",");
+            //System.out.println(Arrays.toString(valores));
+
+            Scanner linhaScanner = new Scanner(linha);
+            linhaScanner.useDelimiter(",");
+
+            linhaScanner.useLocale(Locale.US);
+
+            String tipoConta = linhaScanner.next();
+            int valor2 = linhaScanner.nextInt();
+            int valor3 = linhaScanner.nextInt();
+            String valor4 = linhaScanner.next();
+            double valor5 = linhaScanner.nextDouble();
+
+            String valorFormatado = String.format("%s - %04d-%d, %s: %f", tipoConta, valor2, valor3, valor4, valor5);
+            
+            System.out.println(valorFormatado);
+
+            linhaScanner.close();
+
         }
 
         scanner.close();
